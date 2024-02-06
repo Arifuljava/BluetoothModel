a173d7c0c859649bd072aea65522d91ce93c049a
Ariful0007 <arifulpub143@gmail.com>
2024-02-06 10:28:22 +0600

    Initial Commit

---
+++ BluetoothModel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BluetoothModel
+//
+//  Created by sang on 6/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
